191414dedfb0285ee2c8bdd69609574810efdf48
edraprasetio <edraadrianp@gmail.com>
2025-10-09 11:30:04 -0700

    Add font styles

---
+++ client/src/components/home.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+
+export const Background = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #fcfcfc;
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+        font-family: RobotoMono-Light;
+        font-size: 20px;
+    }
+`
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,35 @@
-import { HomeBackground } from '../components/home/background'
+import { useState } from 'react'
+import { MediumBlackButton } from '../components/atoms/button'
+import { Background } from '../components/home'
 
 export const Home = () => {
+    const [message, setMessage] = useState('')
+    const [sentiment, setSentiment] = useState('')
+
+    async function callOpenAI() {
+        console.log('Call OpenAI API')
+        const res = await fetch('http://localhost:5000/api/sentiment', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ message }),
+        })
+            .then((data) => {
+                return data.json()
+            })
+            .then((data) => {
+                console.log(data)
+                setSentiment(data.choices[0].message.content.trim())
+            })
+    }
+
     return (
         <div>
-            <HomeBackground>greenspace</HomeBackground>
+            <Background>
+                <h1>TARI</h1>
+                <textarea onChange={(e) => setMessage(e.target.value)} placeholder='Paste your message here' />
+                <MediumBlackButton onClick={callOpenAI}>Get the sentiment from OpenAI API</MediumBlackButton>
+                {sentiment !== '' ? <h3>This message is: {sentiment}</h3> : null}
+            </Background>
         </div>
     )
 }
